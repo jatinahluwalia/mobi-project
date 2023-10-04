9c7ee3c76d15c6eb09bc5af9a16e4c5ba43700ba
jatin <jatin.ahluwalia.5@gmail.com>
2023-10-04 11:34:46 +0530

    swagger

--- server/src/routes/product.routes.ts
+++ server/src/routes/product.routes.ts
@@ -11,18 +11,164 @@ import { checkAuth } from "../middlewares/auth";
 
 const productRouter = express.Router();
 
+/**
+ * @openapi
+ * tags:
+ *   name: Products
+ *   description: API for managing products
+ */
+
 //public routes
 
+/**
+ * @openapi
+ * /api/product:
+ *  get:
+ *    summary: Display all products
+ *    description: Display all products
+ *    responses:
+ *      200:
+ *        description: Products fetched successfully
+ *      500:
+ *        description: Some error occurred
+ */
 productRouter.get("/", displayAll);
+
+/**
+ * @openapi
+ * /api/product/{_id}:
+ *  get:
+ *    summary: Display a product by ID
+ *    description: Display a product by ID
+ *    parameters:
+ *      - in: path
+ *        name: _id
+ *        required: true
+ *        description: ID of the product to display
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: Product fetched successfully
+ *      404:
+ *        description: Product not found
+ *      500:
+ *        description: Some error occurred
+ */
 productRouter.get("/:_id", displayOne);
 
 //admin routes
 
+/**
+ * @openapi
+ * /api/product:
+ *  post:
+ *    summary: Add a new product
+ *    description: Add a new product
+ *    security:
+ *      - bearerAuth: []
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *              detail:
+ *                type: string
+ *              hero:
+ *                type: string
+ *              price:
+ *                type: string
+ *              image:
+ *                type: string
+ *    responses:
+ *      201:
+ *        description: Product added successfully
+ *      400:
+ *        description: Invalid request body
+ *      401:
+ *        description: Unauthorized
+ *      500:
+ *        description: Some error occurred
+ */
 productRouter.post("/", checkAuth, isAdminOrSuperAdmin, addProduct);
+
+/**
+ * @openapi
+ * /api/product/{_id}:
+ *  put:
+ *    summary: Update a product by ID
+ *    description: Update a product by ID
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - in: path
+ *        name: _id
+ *        required: true
+ *        description: ID of the product to update
+ *        schema:
+ *          type: string
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *              detail:
+ *                type: string
+ *              hero:
+ *                type: string
+ *              price:
+ *                type: string
+ *              image:
+ *                type: string
+ *    responses:
+ *      200:
+ *        description: Product updated successfully
+ *      400:
+ *        description: Invalid request body
+ *      401:
+ *        description: Unauthorized
+ *      404:
+ *        description: Product not found
+ *      500:
+ *        description: Some error occurred
+ */
 productRouter.put("/:_id", checkAuth, isAdminOrSuperAdmin, updateProduct);
 
 //superadmin routes
 
+/**
+ * @openapi
+ * /api/product/{_id}:
+ *  delete:
+ *    summary: Delete a product by ID
+ *    description: Delete a product by ID
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - in: path
+ *        name: _id
+ *        required: true
+ *        description: ID of the product to delete
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: Product deleted successfully
+ *      401:
+ *        description: Unauthorized
+ *      404:
+ *        description: Product not found
+ *      500:
+ *        description: Some error occurred
+ */
 productRouter.delete("/:_id", checkAuth, isSuperAdmin, deleteProduct);
 
 export default productRouter;
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -17,7 +17,6 @@ import {
   isAdminOrSuperAdmin,
   isSuperAdmin,
 } from "../middlewares/roles";
-
 const userRouter = express.Router();
 
 //Auth routes
@@ -49,7 +48,75 @@ const userRouter = express.Router();
  *        description: Some error occurred
  */
 userRouter.post("/login", login);
+
+/**
+ * @openapi
+ * '/api/user/signup':
+ *  post:
+ *    summary: Signup user
+ *    description: Signup user
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *                required: true
+ *              password:
+ *                type: string
+ *                required: true
+ *              phone:
+ *                type: string
+ *                required: true
+ *              fullName:
+ *                type: string
+ *                required: true
+ *    responses:
+ *      200:
+ *        description: User signed up successfully
+ *      406:
+ *        description: Invalid credentials
+ *      500:
+ *        description: Some error occurred
+ */
 userRouter.post("/signup", signup);
+
+/**
+ * @openapi
+ * '/api/user/signup-admin':
+ *  post:
+ *    summary: Signup admin
+ *    security:
+ *      bearerAuth: []
+ *    description: Signup admin
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *                required: true
+ *              password:
+ *                type: string
+ *                required: true
+ *              phone:
+ *                type: string
+ *                required: true
+ *              fullName:
+ *                type: string
+ *                required: true
+ *    responses:
+ *      200:
+ *        description: Admin signed up successfully
+ *      406:
+ *        description: Invalid credentials
+ *      500:
+ *        description: Some error occurred
+ */
 userRouter.post("/signup-admin", checkAuth, isAdminOrSuperAdmin, signupAdmin);
 userRouter.post(
   "/signup-super-admin",
@@ -57,6 +124,301 @@ userRouter.post(
   isSuperAdmin,
   signupSuperAdmin
 );
+
+/**
+ * @openapi
+ * /api/user/login:
+ *  post:
+ *    summary: Login user
+ *    description: Login user
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *                required: true
+ *              password:
+ *                type: string
+ *                required: true
+ *    responses:
+ *      200:
+ *        description: User logged in successfully
+ *      400:
+ *        description: Invalid credentials
+ *      500:
+ *        description: Some error occurred
+ */
+userRouter.post("/login", login);
+
+/**
+ * @openapi
+ * /api/user/signup:
+ *  post:
+ *    summary: Signup user
+ *    description: Signup user
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *                required: true
+ *              password:
+ *                type: string
+ *                required: true
+ *              phone:
+ *                type: string
+ *                required: true
+ *              fullName:
+ *                type: string
+ *                required: true
+ *    responses:
+ *      200:
+ *        description: User signed up successfully
+ *      406:
+ *        description: Invalid credentials
+ *      500:
+ *        description: Some error occurred
+ */
+userRouter.post("/signup", signup);
+
+/**
+ * @openapi
+ * /api/user/signup-admin:
+ *  post:
+ *    summary: Signup admin
+ *    security:
+ *      bearerAuth: []
+ *    description: Signup admin
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *                required: true
+ *              password:
+ *                type: string
+ *                required: true
+ *              phone:
+ *                type: string
+ *                required: true
+ *              fullName:
+ *                type: string
+ *                required: true
+ *    responses:
+ *      200:
+ *        description: Admin signed up successfully
+ *      406:
+ *        description: Invalid credentials
+ *      500:
+ *        description: Some error occurred
+ */
+userRouter.post("/signup-admin", checkAuth, isAdminOrSuperAdmin, signupAdmin);
+
+/**
+ * @openapi
+ * /api/user/signup-super-admin:
+ *  post:
+ *    summary: Signup super admin
+ *    security:
+ *      bearerAuth: []
+ *    description: Signup super admin
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *                required: true
+ *              password:
+ *                type: string
+ *                required: true
+ *              phone:
+ *                type: string
+ *                required: true
+ *              fullName:
+ *                type: string
+ *                required: true
+ *    responses:
+ *      200:
+ *        description: Super admin signed up successfully
+ *      406:
+ *        description: Invalid credentials
+ *      500:
+ *        description: Some error occurred
+ */
+userRouter.post(
+  "/signup-super-admin",
+  checkAuth,
+  isSuperAdmin,
+  signupSuperAdmin
+);
+
+/**
+ * @openapi
+ * /api/user:
+ *  get:
+ *    summary: Get user profile
+ *    description: Get user profile
+ *    security:
+ *      bearerAuth: []
+ *    responses:
+ *      200:
+ *        description: User profile retrieved successfully
+ *      401:
+ *        description: Unauthorized
+ *      500:
+ *        description: Some error occurred
+ */
+userRouter.get("/", checkAuth, profile);
+
+/**
+ * @openapi
+ * /api/user:
+ *  put:
+ *    summary: Update user profile
+ *    description: Update user profile
+ *    security:
+ *      bearerAuth: []
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *              password:
+ *                type: string
+ *              phone:
+ *                type: string
+ *              fullName:
+ *                type: string
+ *    responses:
+ *      200:
+ *        description: User profile updated successfully
+ *      401:
+ *        description: Unauthorized
+ *      500:
+ *        description: Some error occurred
+ */
+userRouter.put("/", checkAuth, updateSelf);
+
+/**
+ * @openapi
+ * /api/user:
+ *  delete:
+ *    summary: Delete user profile
+ *    description: Delete user profile
+ *    security:
+ *      bearerAuth: []
+ *    responses:
+ *      200:
+ *        description: User profile deleted successfully
+ *      401:
+ *        description: Unauthorized
+ *      500:
+ *        description: Some error occurred
+ */
+userRouter.delete("/", checkAuth, deleteSelf);
+
+/**
+ * @openapi
+ * /api/user/all:
+ *  get:
+ *    summary: Get all users
+ *    description: Get all users
+ *    security:
+ *      bearerAuth: []
+ *    responses:
+ *      200:
+ *        description: All users retrieved successfully
+ *      401:
+ *        description: Unauthorized
+ *      403:
+ *        description: Forbidden
+ *      500:
+ *        description: Some error occurred
+ */
+userRouter.get("/all", checkAuth, isSuperAdmin, showAll);
+
+/**
+ * @openapi
+ * /api/user/{id}:
+ *  put:
+ *    summary: Update other user
+ *    description: Update other user
+ *    security:
+ *      bearerAuth: []
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: User ID
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *              password:
+ *                type: string
+ *              phone:
+ *                type: string
+ *              fullName:
+ *                type: string
+ *    responses:
+ *      200:
+ *        description: User updated successfully
+ *      401:
+ *        description: Unauthorized
+ *      403:
+ *        description: Forbidden
+ *      500:
+ *        description: Some error occurred
+ */
+userRouter.put("/:id", checkAuth, isAdminOrSuperAdmin, updateOtherUser);
+
+/**
+ * @openapi
+ * /api/user/{id}:
+ *  delete:
+ *    summary: Delete other user
+ *    description: Delete other user
+ *    security:
+ *      bearerAuth: []
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: User ID
+ *    responses:
+ *      200:
+ *        description: User deleted successfully
+ *      401:
+ *        description: Unauthorized
+ *      403:
+ *        description: Forbidden
+ *      500:
+ *        description: Some error occurred
+ */
+userRouter.delete("/:id", checkAuth, isAdmin, deleteOtherUser);
 
 //User specific routes
 
